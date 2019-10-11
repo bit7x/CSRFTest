09717f9a882e32d49b2f8f2dae1d5749bc27265a
Viranja Hirunika <viranja@live.com>
2019-10-11 11:08:32 +0530

    orgnaize imports

--- src/main/java/com/csrf/test/controller/UserController.java
+++ src/main/java/com/csrf/test/controller/UserController.java
@@ -3,16 +3,9 @@ package com.csrf.test.controller;
 import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
